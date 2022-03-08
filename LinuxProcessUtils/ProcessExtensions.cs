e6292633cc868a3e5ca1c7d1cad4bd2730e639ac
Christopher <chris.mcmorran@carleton.ca>
2022-03-08 13:09:10 -0500

    Bad implementation for reading arrays with multiple libc calls. This needs to improve.

--- LinuxProcessUtils/ProcessExtensions.cs
+++ LinuxProcessUtils/ProcessExtensions.cs
@@ -26,6 +26,19 @@ public static class ProcessExtensions
         return res != -1;
     }
     
+    public static unsafe T[] Read<T>(this Process process, IntPtr address, int length = 1) where T : unmanaged
+    {
+        var value = new T[length];
+        for (int i = 0; i < length; i++)
+        {
+            var pointer = new IntPtr(address.ToInt64() + i);
+            Read<T>(process, pointer, out value[i]);
+        }
+
+        return value;
+    }
+
+    
     public static unsafe bool Write<T>(Process process, T value, IntPtr address) where T : unmanaged
     {
         var ptr = &value;
